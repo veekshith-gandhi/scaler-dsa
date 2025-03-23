9245992caecb8f7d13bc53f827e39bbbba95a870
veekshith <veekshithgandhi@gmail.com>
2025-03-23 12:35:46 +0530

    time complexity

--- binarysearch/searchForK.js
+++ binarysearch/searchForK.js
@@ -1,6 +1,10 @@
+//GIven sorted array with distinct element search index of an element K
+//if not return -1
+
+//Brute Force : linear search   TC  : O(N)
+
 const array = [3,6,9,12,14,19,23,26,35,56,90];
 const K = 129;
-
 
 const searchForK = (array,k) =>{
     let left = 0;
@@ -22,3 +26,4 @@ const searchForK = (array,k) =>{
 
 const result  = searchForK(array,K);
 console.log("RESULT --------------",result)
+//BINARY SEARCH : TC : O(logN)
